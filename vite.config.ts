22912eea5daf637f37f0788d03c73c39b6dfd340
lizgar <liz17_9@hotmail.com>
2026-05-04 19:30:59 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/DesignareSemillero/',
+    base: '/Responsive-AR-Portfolio-Section/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
